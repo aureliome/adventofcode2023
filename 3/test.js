b122bf36572dc6a0a1b91a8b7d4df1cb5e3ef53a
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-03 11:27:02 +0100

    test(3rd): added first puzzle test

---
+++ 3/test.js
@@ -0,0 +1,25 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const first = require("./1");
+// const second = require("./2");
+
+const input = `
+  467..114..
+  ...*......
+  ..35..633.
+  ......#...
+  617*......
+  .....+.58.
+  ..592.....
+  ......755.
+  ...$.*....
+  .664.598..
+`;
+
+test("first puzzle works", () => {
+  assert.strictEqual(first(input), 4361);
+});
+
+// test("second puzzle works", () => {
+//   assert.strictEqual(second(input), 2286);
+// });
