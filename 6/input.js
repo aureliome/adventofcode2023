ecea7ec4233f4d5193eb1f98aa66556946c16e17
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-06 16:36:02 +0100

    chore(6th): added 1st puzzle text and expected value

---
+++ 6/input.js
@@ -0,0 +1,7 @@
+// TODO: insert here the real input of the puzzles
+
+const input = `
+hello world
+`;
+
+module.exports = input;
---
+++ 6/test.js
@@ -0,0 +1,21 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const first = require("./1");
+const second = require("./2");
+
+/*
+  TODO: insert here the test input of the puzzles
+*/
+const input = `
+hello moon
+`;
+
+test("first puzzle works", () => {
+  // TODO: replace this value with the expected value
+  assert.strictEqual(first(input), 288);
+});
+
+test("second puzzle works", () => {
+  // TODO: replace this value with the expected value
+  assert.strictEqual(second(input), 42);
+});
