f3a6b8a291670bba882bada67e67f18d25e35c92
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-06 16:31:46 +0100

    chore: added puzzle template

---
+++ template/2.js
@@ -0,0 +1,24 @@
+/*
+  TODO: insert here the problem of the second puzzle
+*/
+
+const realInput = require("./input");
+
+const main = (input) => {
+  /*
+    TODO: insert here the implementation of the second puzzle
+    using the "input" parameter
+  */
+
+  /*
+    TODO: this value with:
+    1. at the beginning, the expected value in the test
+    2. when the implementation will be ready, the calculated value
+  */
+  return 42;
+};
+
+// TODO: uncomment this line when you're ready to test it with real input
+// console.log(`The sum is ${main(realInput)}`);
+
+module.exports = main;
