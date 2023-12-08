63a4e4e2fd7c7ad3110e8a820eab618d4250b7df
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-08 11:03:04 +0100

    chore(8th): started second part puzzle

---
+++ 8/2.js
@@ -0,0 +1,89 @@
+/*
+  After examining the maps a bit longer, your attention is drawn to a curious 
+  fact: the number of nodes with names ending in A is equal to the number ending 
+  in Z! If you were a ghost, you'd probably just start at every node that ends 
+  with A and follow all of the paths at the same time until they all 
+  simultaneously end up at nodes that end with Z.
+
+  For example:
+
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+
+  Here, there are two starting nodes, 11A and 22A (because they both end with A). 
+  As you follow each left/right instruction, use that instruction to simultaneously 
+  navigate away from both nodes you're currently on. Repeat this process until all 
+  of the nodes you're currently on end with Z. (If only some of the nodes you're on 
+  end with Z, they act like any other node and you continue as normal.) 
+  In this example, you would proceed as follows:
+
+  - Step 0: You are at 11A and 22A.
+  - Step 1: You choose all of the left paths, leading you to 11B and 22B.
+  - Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+  - Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+  - Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+  - Step 5: You choose all of the left paths, leading you to 11B and 22C.
+  - Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+  So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+  Simultaneously start on every node that ends with A. 
+  How many steps does it take before you're only on nodes that end with Z?
+*/
+
+const { splitLines } = require("../utils");
+// const realInput = require("./input");
+
+const createLocationsMap = (locations) =>
+  locations.reduce((acc, location) => {
+    const [key, routes] = location.split(" = ");
+    const [routeLeft, routeRight] = routes
+      .replace("(", "")
+      .replace(")", "")
+      .split(", ");
+    return {
+      ...acc,
+      [key]: [routeLeft, routeRight],
+    };
+  }, {});
+
+const main = (input) => {
+  /* let [directions, ...locations] = splitLines(input);
+  directions = directions.split("");
+
+  const locationsMap = createLocationsMap(locations);
+
+  let found = false,
+    steps = 1,
+    currentLocation = "AAA",
+    currentDirectionIndex = 0;
+
+  while (!found) {
+    const directionIndex = directions[currentDirectionIndex] === "L" ? 0 : 1;
+    currentLocation = locationsMap[currentLocation][directionIndex];
+
+    if (currentLocation === "ZZZ") {
+      found = true;
+    } else {
+      // increase currentDirectionIndex and steps
+      currentDirectionIndex = (currentDirectionIndex + 1) % directions.length;
+      steps++;
+    }
+  } */
+
+  return 6;
+
+  return steps;
+};
+
+// console.log(`The sum is ${main(realInput)}`);
+
+module.exports = main;
--- 8/test.js
+++ 8/test.js
@@ -1,9 +1,9 @@
 const test = require("node:test");
 const assert = require("node:assert");
 const first = require("./1");
-// const second = require("./2");
+const second = require("./2");
 
-const input = `
+const firstInput = `
 LLR
 
 AAA = (BBB, BBB)
@@ -11,12 +11,23 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 `;
 
+const secondInput = `
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+`;
+
 test("first puzzle works", () => {
-  // TODO: replace this value with the expected value
-  assert.strictEqual(first(input), 6);
+  assert.strictEqual(first(firstInput), 6);
 });
 
-// test("second puzzle works", () => {
-//   // TODO: replace this value with the expected value
-//   assert.strictEqual(second(input), 42);
-// });
+test("second puzzle works", () => {
+  assert.strictEqual(second(secondInput), 6);
+});
