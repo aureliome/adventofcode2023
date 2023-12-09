52d8440a85fae4d89643676628ee13711a3669bb
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-09 14:59:48 +0100

    chore(9th): tested real input in first puzzle

--- 9/1.js
+++ 9/1.js
@@ -142,7 +142,6 @@ const main = (input) => {
   return sum;
 };
 
-// TODO: uncomment this line when you're ready to test it with real input
-// console.log(`The sum is ${main(realInput)}`);
+console.log(`The sum is ${main(realInput)}`);
 
 module.exports = main;
