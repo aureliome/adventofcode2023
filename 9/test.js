d0ce59471d9bbce3fd9c228190c6ada5419db096
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-09 14:19:37 +0100

    chore(9th): added 1st puzzle text + expected value

---
+++ 9/test.js
@@ -0,0 +1,19 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const first = require("./1");
+// const second = require("./2");
+
+const input = `
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+`;
+
+test("first puzzle works", () => {
+  assert.strictEqual(first(input), 68);
+});
+
+// test("second puzzle works", () => {
+//   // TODO: replace this value with the expected value
+//   assert.strictEqual(second(input), 42);
+// });
